818779055a0e96d32c102b7e254fd00bcd7e8e98
jotaseme <Jose@Jose-HP>
2016-02-09 21:24:04 +0100

    inicializacion proyecto RESTfull API

---
+++ codigo/laravel/app/Categoria.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categoria extends Model
+{
+    protected $table = 'categorias';
+
+    protected $fillable = array('nombre'):
+    protected $hidden = ['created_at','update_at'];
+
+    public function productos()
+    {
+    	return $this->hasMany(App\Productos);
+    }
+}
---
+++ codigo/laravel/app/Producto.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    
+    protected $table = 'productos';
+
+    protected $fillable = array('nombre','categoria_id'):
+    protected $hidden = ['created_at','update_at'];
+
+    public function categoria()
+    {
+    	return $this->belongsTo(App\Categoria);
+    }
+}
