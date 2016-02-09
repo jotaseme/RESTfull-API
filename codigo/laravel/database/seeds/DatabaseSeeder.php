26945be84d435d7a889e9fbddacba1606d0610ce
jotaseme <Jose@Jose-HP>
2016-02-09 21:37:19 +0100

    configuracion seeder
    se añaden las llamadas seeder para los modelos Categoria y Producto

--- codigo/laravel/database/seeds/DatabaseSeeder.php
+++ codigo/laravel/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserTableSeeder::class);
+        $this->call(CategoriaSeeder::class);
+        $this->call(ProductoSeeder::class);
+
     }
 }
